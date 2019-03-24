74121cb4811dbf2ce29c5a922e1091cbfd4ec5c6
Guillermo Gallegos <guillermogallegos@utexas.edu>
2019-03-24 16:24:32 -0500

    Added sample data file

---
+++ idb/data.py
@@ -0,0 +1,17 @@
+pokemon = ["Ekans", "Arbok", "Sandshrew", "Sandslash", "Magikarp", "Gyarados"]
+info = ["ID", "Description", "Category", "Type", "Weakness", "Evolution"]
+
+data = {"Ekans": [{"ID": "023"}, {"Description": "Snake that is 3 meters long and weighs around 6.9 kilograms."},
+       {"Category": "Snake"}, {"Type": "Poison Type"}, {"Weakness": "Ground, Psychic"}, {"Evolution": "Evolves to Arbok"}]
+        , "Arbok": [{"ID": "024"}, {"Description": "Cobra that is 3.5 meters long and weighs around 65 kilograms."},
+       {"Category": "Cobra"}, {"Type": "Poison Type"}, {"Weakness": "Ground, Psychic"}, {"Evolution": "Evolves from Ekansk"}],
+        "Sandshrew": [{"ID": "027"}, {"Description": "Mouse that is 0.6 meters tall and weighs around 12 kilograms."},
+                  {"Category": "Mouse"}, {"Type": "Ground Type"}, {"Weakness": "Grass, Ice, Water"},
+                  {"Evolution": "Evolves to Sandslash"}],
+        "Sandslash": [{"ID": "028"}, {"Description": "Mouse that is 1 meters tall and weighs around 29.5 kilograms"}, 
+        {"Category": "Mouse"}, {"Type": "Ground Type"}, {"Weakness": "Grass, Ice, Water"}, {"Evolution": "Evolves from Sandshrew"}],
+        "Magikarp": [{"ID": "129"}, {"Description": "Fish that is 0.9 meters long and weighs around 10.0 kilograms"}, 
+        {"Category": "Fish"}, {"Type": "Water Type"}, {"Weakness": "Grass, Electric"}, {"Evolution": "Evolves to Gyarados"}],
+        "Gyarados": [{"ID": "130"}, {"Description": "Atrocious Pokémon that is 6.5 meters tall and weighs around 235.0 kilograms"}, 
+        {"Category": "Atrocious Pokémon"}, {"Type": "Water and Flying Type"}, {"Weakness": "Rock, Electric"}, {"Evolution": "Evolves from Magikarp"}],
+        }
