506aec2b1601fb5b0cb475e6c1a41fa00141c120
Jacob Lubecki <jacoblubecki@gmail.com>
2019-03-23 19:32:35 -0500

    Add temporary flask HTML routing

--- idb/__init__.py
+++ idb/__init__.py
@@ -4,33 +4,36 @@ Flask basics: http://flask.pocoo.org/docs/1.0/quickstart/#a-minimal-application
 """
 
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
 # App initialization must happen first.
 app = Flask(__name__)
 
-#-----------------
-# ROUTE DECORATORS
-#-----------------
-## Create route decorators which maps URL route to corresponding HTML template
-## to serve thr user.
 
 @app.route('/')
 def index():
     """Routes to the index page of the site."""
     return render_template('index.html')
 
+
 @app.route('/data')
 def example():
     """Routes to a sample data page."""
     return render_template('datapage.html')
 
-#------------------------
-# END ROUTE DECORATORS
-#------------------------
+
+@app.route('/entity')
+def entity():
+    """Routes to a specific entity. This will be an API_get"""
+    return render_template('entity.html')
 
 
-# Debug provides live updates without restarting;
-# this is a development only argument.
+@app.route('/indv')
+def indv():
+    """Individual view will return a page that gets its name from the API"""
+    return render_template('individual_view.html')
+
+
 if __name__ == "__main__":
+    # Debug provides live updates without restarting;
+    # this is a development only argument.
     app.run(debug=True)
