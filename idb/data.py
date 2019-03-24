83aca87f0e96311e427208863823d07655efd0df
tchinn <tchinn425@gmail.com>
2019-03-24 18:40:59 -0500

    Update dictionaries in data.py

--- idb/data.py
+++ idb/data.py
@@ -1,17 +1,101 @@
-pokemon = ["Ekans", "Arbok", "Sandshrew", "Sandslash", "Magikarp", "Gyarados"]
-info = ["ID", "Description", "Category", "Type", "Weakness", "Evolution"]
+POKEMON = {"ekans": {
+            "id": "023",
+            "description": "Snake that is 3 meters long and weighs around 6.9 kilograms.",
+            "category": "Snake",
+            "type": ["poison"]},
+          "arbok": {
+            "id": "024",
+            "description": "Cobra that is 3.5 meters long and weighs around 65 kilograms.",
+            "category": "Cobra",
+            "type": ["poison"]},
+          "sandshrew": {
+            "id": "027",
+            "description": "Mouse that is 0.6 meters tall and weighs around 12 kilograms.",
+            "category": "Mouse",
+            "type": ["ground"]},
+          "sandslash": {
+            "id": "028",
+            "description": "Mouse that is 1 meters tall and weighs around 29.5 kilograms.",
+            "category": "Mouse",
+            "type": ["ground"]},
+          "magikarp": {
+            "id": "129",
+            "description": "Fish that is 0.9 meters long and weighs around 10.0 kilograms.",
+            "category": "Fish",
+            "type": ["water"]},
+          "gyarados": {
+            "id": "130",
+            "description": "Atrocious Pokémon that is 3.5 meters long and weighs around 65 kilograms.",
+            "category": "Atrocious Pokémon",
+            "type": ["water", "flying"]}}
 
-data = {"Ekans": [{"ID": "023"}, {"Description": "Snake that is 3 meters long and weighs around 6.9 kilograms."},
-       {"Category": "Snake"}, {"Type": "Poison Type"}, {"Weakness": "Ground, Psychic"}, {"Evolution": "Evolves to Arbok"}]
-        , "Arbok": [{"ID": "024"}, {"Description": "Cobra that is 3.5 meters long and weighs around 65 kilograms."},
-       {"Category": "Cobra"}, {"Type": "Poison Type"}, {"Weakness": "Ground, Psychic"}, {"Evolution": "Evolves from Ekansk"}],
-        "Sandshrew": [{"ID": "027"}, {"Description": "Mouse that is 0.6 meters tall and weighs around 12 kilograms."},
-                  {"Category": "Mouse"}, {"Type": "Ground Type"}, {"Weakness": "Grass, Ice, Water"},
-                  {"Evolution": "Evolves to Sandslash"}],
-        "Sandslash": [{"ID": "028"}, {"Description": "Mouse that is 1 meters tall and weighs around 29.5 kilograms"}, 
-        {"Category": "Mouse"}, {"Type": "Ground Type"}, {"Weakness": "Grass, Ice, Water"}, {"Evolution": "Evolves from Sandshrew"}],
-        "Magikarp": [{"ID": "129"}, {"Description": "Fish that is 0.9 meters long and weighs around 10.0 kilograms"}, 
-        {"Category": "Fish"}, {"Type": "Water Type"}, {"Weakness": "Grass, Electric"}, {"Evolution": "Evolves to Gyarados"}],
-        "Gyarados": [{"ID": "130"}, {"Description": "Atrocious Pokémon that is 6.5 meters tall and weighs around 235.0 kilograms"}, 
-        {"Category": "Atrocious Pokémon"}, {"Type": "Water and Flying Type"}, {"Weakness": "Rock, Electric"}, {"Evolution": "Evolves from Magikarp"}],
-        }
+EVOLUTION = {
+      "magikarp": {
+            'from': None,
+            'to': ['gyarados'],
+            'min_level': 20,
+            'gender': None,
+            'hold_item': None,
+      },
+      "gyarados": {
+            'from': ['magikarp'],
+            'to': None,
+            'min_level': None,
+            'gender': None,
+            'hold_item': None,
+      },
+      "ekans": {
+            'from': None,
+            'to': ['arbok'],
+            'min_level': 22,
+            'gender': None,
+            'hold_item': None,
+      },
+      "arbok": {
+            'from': ['ekans'],
+            'to': None,
+            'min_level': None,
+            'gender': None,
+            'hold_item': None,
+      },
+      "sandshrew": {
+            'from': None,
+            'to': ['sandslash'],
+            'min_level': 22,
+            'gender': None,
+            'hold_item': None,
+      },
+      "sandslash": {
+            'from': ['sandshrew'],
+            'to': None,
+            'min_level': None,
+            'gender': None,
+            'hold_item': None,
+      }
+
+}
+
+TYPE = {
+    'water': {
+          'advantages': ['ground', 'rock', 'fire']
+          'weaknesses': ['grass', 'electric']
+          'pokemon': ['magikarp', 'gyarados']
+    },
+    'flying': {
+          'advantages': ['fighting', 'bug', 'grass']
+          'weaknesses': ['rock', 'electric', 'ice']
+          'pokemon': ['gyarados']
+    },
+    'ground': {
+          'advantages': ['poison', 'rock', 'fire', 'steel', 'electric']
+          'weaknesses': ['water', 'grass', 'ice']
+          'pokemon': ['sandslash', 'sandshrew']
+    },
+    'poison': {
+          'advantages': ['grass', 'fairy']
+          'weaknesses': ['ground', 'psychic']
+          'pokemon': ['ekans', 'arbok']
+    },
+}
+
+
