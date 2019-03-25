9f8b4f46db34fad039d6c90aff94d23ddf991b10
ZackerWins <zawarnick1@gmail.com>
2019-03-25 03:18:35 -0500

    Add routes and sources for types pages

---
+++ frontend/src/views/Type.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="Type" style="padding-left">
+    <Navbar/>
+    <div class="container">
+      <h1>Individual Type</h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <table>
+            <tr>
+              <th><h3>Strong Against</h3></th>
+            </tr>
+            <tr>
+              Ice
+            </tr>
+            <br/>
+            <tr>
+              Water
+            </tr>
+          </table>
+        </div>
+        <div class="col-sm">
+          <table>
+            <tr>
+              <th><h3>Weak Against</h3></th>        
+            </tr>
+            <tr>
+              Rock
+            </tr>
+            <br/>
+            <tr>
+              Steel
+            </tr>        
+          </table>
+        </div>
+        <div class="col-sm">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Gatto_europeo4.jpg/250px-Gatto_europeo4.jpg">
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md" style="text-align: left; padding-left: 10px">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta illo velit error ex? Atque quasi vel iste iure voluptatibus. Similique delectus quam expedita eius, dolore optio at nobis ab dolorem?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus velit exercitationem omnis ad eos. Nesciunt repudiandae consequatur vitae molestiae omnis pariatur dolorem delectus, minus perferendis nisi maxime similique ducimus et?
+          </div>
+      </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+        <CardItem
+          title="Card 1"
+          msg="Message for card 1."
+          link="/about"
+          link_text="About"
+        />
+       </div>             
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import CardItem from "@/components/CardItem.vue";
+
+export default {
+  name: "About",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style scoped>
+  .container {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    background-color: aqua
+  };
+  .table {
+    text-align: center
+  }
+</style>
---
+++ frontend/src/views/TypeList.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="Types">
+    <Navbar/>
+    <h1>List of Types</h1>
+    <div class="container" style="padding-left:200px">
+      <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+      <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+            <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+            <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+            <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+            <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+            <div class="row">
+        <CardItem
+          title="Fire"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Water"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+        <CardItem
+          title="Ice"
+          msg="Message for card 1."
+          link="/type"
+          link_text="About"
+        />
+      </div> 
+      <div class="row"><p></p></div> <!--Bodged together, fix later-->
+    </div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import CardItem from "@/components/CardItem.vue";
+
+export default {
+  name: "Types",
+  components: {
+    Navbar,
+    CardItem
+  }
+};
+</script>
