f91aa241e185da8ed48e15897b96c3fd6b17cc1c
tchinn <tchinn425@gmail.com>
2019-03-24 19:39:25 -0500

    Fix quotation formatting and id values

--- idb/data.py
+++ idb/data.py
@@ -1,71 +1,71 @@
-POKEMON = {"ekans": {
-            "id": "023",
-            "description": "Snake that is 3 meters long and weighs around 6.9 kilograms.",
-            "category": "Snake",
-            "type": ["poison"]},
-          "arbok": {
-            "id": "024",
-            "description": "Cobra that is 3.5 meters long and weighs around 65 kilograms.",
-            "category": "Cobra",
-            "type": ["poison"]},
-          "sandshrew": {
-            "id": "027",
-            "description": "Mouse that is 0.6 meters tall and weighs around 12 kilograms.",
-            "category": "Mouse",
-            "type": ["ground"]},
-          "sandslash": {
-            "id": "028",
-            "description": "Mouse that is 1 meters tall and weighs around 29.5 kilograms.",
-            "category": "Mouse",
-            "type": ["ground"]},
-          "magikarp": {
-            "id": "129",
-            "description": "Fish that is 0.9 meters long and weighs around 10.0 kilograms.",
-            "category": "Fish",
-            "type": ["water"]},
-          "gyarados": {
-            "id": "130",
-            "description": "Atrocious Pokémon that is 3.5 meters long and weighs around 65 kilograms.",
-            "category": "Atrocious Pokémon",
-            "type": ["water", "flying"]}}
+POKEMON = {'ekans': {
+            'id': 23,
+            'description': 'Snake that is 3 meters long and weighs around 6.9 kilograms.',
+            'category': 'Snake',
+            'type': ['poison']},
+          'arbok': {
+            'id': 24,
+            'description': 'Cobra that is 3.5 meters long and weighs around 65 kilograms.',
+            'category': 'Cobra',
+            'type': ['poison']},
+          'sandshrew': {
+            'id': 27,
+            'description': 'Mouse that is 0.6 meters tall and weighs around 12 kilograms.',
+            'category': 'Mouse',
+            'type': ['ground']},
+          'sandslash': {
+            'id': 28,
+            'description': 'Mouse that is 1 meters tall and weighs around 29.5 kilograms.',
+            'category': 'Mouse',
+            'type': ['ground']},
+          'magikarp': {
+            'id': 129,
+            'description': 'Fish that is 0.9 meters long and weighs around 10.0 kilograms.',
+            'category': 'Fish',
+            'type': ['water']},
+          'gyarados': {
+            'id': 130,
+            'description': 'Atrocious Pokémon that is 3.5 meters long and weighs around 65 kilograms.',
+            'category': 'Atrocious Pokémon',
+            'type': ['water', 'flying']}}
 
 EVOLUTION = {
-      "magikarp": {
+      'magikarp': {
             'from': None,
             'to': ['gyarados'],
             'min_level': 20,
             'gender': None,
             'hold_item': None,
       },
-      "gyarados": {
+      'gyarados': {
             'from': ['magikarp'],
             'to': None,
             'min_level': None,
             'gender': None,
             'hold_item': None,
       },
-      "ekans": {
+      'ekans': {
             'from': None,
             'to': ['arbok'],
             'min_level': 22,
             'gender': None,
             'hold_item': None,
       },
-      "arbok": {
+      'arbok': {
             'from': ['ekans'],
             'to': None,
             'min_level': None,
             'gender': None,
             'hold_item': None,
       },
-      "sandshrew": {
+      'sandshrew': {
             'from': None,
             'to': ['sandslash'],
             'min_level': 22,
             'gender': None,
             'hold_item': None,
       },
-      "sandslash": {
+      'sandslash': {
             'from': ['sandshrew'],
             'to': None,
             'min_level': None,
