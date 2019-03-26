dc227f5a7f605f31987eafaff526e167b345670f
Guillermo Gallegos <guillermogallegos@utexas.edu>
2019-03-26 12:17:15 -0500

    Removed extra type cards

--- frontend/src/views/TypeList.vue
+++ frontend/src/views/TypeList.vue
@@ -14,216 +14,12 @@
           <div class="col-md-4">
             <div class="card mb-4 shadow-sm">
               <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://res.cloudinary.com/lmn/image/upload/c_limit,h_360,w_640/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/m/a/x/maxresdefault-45bee.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Bug</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Bug"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
               <img class="card-img-top" src="https://i.pinimg.com/originals/b0/bc/69/b0bc69eacf147a54ec4e5c4cacdf81db.jpg">
               <div class="card-body">
                 <p class="card-text"><b>Water</b></p>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                     <a href="/types/Water"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/0dwjy-0uFVI/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Dragon</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Dragon"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/cPyNhxB_3cM/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Electric</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Electric"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://s1-ssl.dmcdn.net/mTInY/x1080-Q_X.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Rock</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Rock"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="http://i.ytimg.com/vi/uiv5GEd0Zzc/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Fighting</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Fighting"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-            <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/NW0rNI1sYQM/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Fire</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Fire"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="http://lifeinspiringquotes.com/wp-content/uploads/2018/08/terrific-pokemon-normal-type-coloring-photos-of-amusing-all-flying-type-pok-mon-youtube-page.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Flying</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Flying"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/9THsZhlJU6k/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Ghost</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Ghost"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/rRmcJ7PgTQ0/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Grass</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Grass"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://d2juyu303oh9b6.cloudfront.net/image/152367997cae9bab386e4c45237f347b.jpg?&icq=80&sig=def4fe4660a040f5b8e270c031a0dd94">
-              <div class="card-body">
-                <p class="card-text"><b>Ground</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Ground"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/EheZsIMZnKo/maxresdefault.jpg">
-              <div class="card-body">
-                <p class="card-text"><b>Ice</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Ice"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-              <img class="card-img-top" src="https://elmundodelcine.com/wp-content/uploads/2018/12/normal-type-pokemon-13.jpg">
-              <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <div class="card-body">
-                <p class="card-text"><b>Normal</b></p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <a href="/types/Normal"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
                   </div>
                 </div>
               </div>
@@ -245,15 +41,16 @@
             </div>
           </div>
 
+
           <div class="col-md-4">
             <div class="card mb-4 shadow-sm">
               <!--<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>-->
-              <img class="card-img-top" src="https://i.ytimg.com/vi/CyFxRZoJAZU/maxresdefault.jpg">
+              <img class="card-img-top" src="https://d2juyu303oh9b6.cloudfront.net/image/152367997cae9bab386e4c45237f347b.jpg?&icq=80&sig=def4fe4660a040f5b8e270c031a0dd94">
               <div class="card-body">
-                <p class="card-text"><b>Psychic</b></p>
+                <p class="card-text"><b>Ground</b></p>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
-                    <a href="/types/Psychic"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                    <a href="/types/Ground"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
                   </div>
                 </div>
               </div>
@@ -262,6 +59,8 @@
         </div>
       </div>
     </div>
+
+
 
   </div>
 
