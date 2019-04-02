6e560bddda6fe1477cd4e824529bc320781ef5db
Zack Warnick <zawarnick1@gmail.com>
2019-04-02 02:59:17 -0500

    add animations to splash

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,95 +1,193 @@
 <template>
-  <div class="home">
-    <Navbar />
-    <div class="spacer">
-    <div class="bg">
-   
-    <div class="container">
-      <div class="jumbotron">
-        <h1 class="display-4">Welcome to PokéDB</h1>
-        <p class="lead">
-          This is an indexed database of all things Pokémon. 
-          Click below to find out imformation on the Pokeémon, types, evolution patterns, and so much more!
-        </p>
 
-        <!--
-        <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button"
-            >Learn more</a
-          >
-        </p>
-        -->
+  <div class="home">
+    <div class="fade-container">
+    <Navbar/>
+    <div @click="clicked=!clicked">
+      <div class="bg">
+        <div class="container text-scroll-bg">
+        <transition onload name ="PokeIn">
+          <img src="../assets/PokeBallUpright.png" class="upright"/>
+        </transition>
+
+        <div class="container-text">
+          
+          <div class="hero-text">POKEDB</div>
+          <div class="row">
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, saepe? Dignissimos laudantium,
+              a accusamus explicabo illo distinctio, ipsam soluta quo qui minus voluptatum at labore ab ullam magni ea expedita.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, saepe? Dignissimos laudantium,
+              a accusamus explicabo illo distinctio, ipsam soluta quo qui minus voluptatum at labore ab ullam magni ea expedita.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, saepe? Dignissimos laudantium,</p>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="container">
-      <div class="row">
-        <CardItem
-          title="Pokémon"
-          msg="Learn about the various Pokémon across all the games."
-          link="/pokemon"
-          link_text="Go"
-        />
-        <CardItem
-          title="Types"
-          msg="Learn about the 18 standard types, or the 3 hidden and special types."
-          link="/types"
-          link_text="Go"
-        />
-        <CardItem
-          title="Evolutions"
-          msg="Learn about the paths each Pokémon takes to evolve."
-          link="/evolutions/"
-          link_text="Go"
-        />
       </div>
     </div>
     </div>
-  </div>
   </div>
 </template>
 
+
+<style scoped>
+
+  .hero-te {
+    z-index: 1;
+    position: absolute;
+  }
+  .bg {
+    padding-top: 0;
+    padding-bottom: 0%;
+    margin-bottom: 0px;
+    /* The image used */
+    background-image: url('../assets/home-background.jpg');
+  
+    /* Full height */
+    height: 100%;
+  
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+
+    align-content: center;
+
+    position: sticky;
+      }
+  
+  .container-text {
+    opacity: 0;
+    transform: translateY(-250%);
+    padding: 25px;
+    animation: fadeInAfterPoke 1s 1 forwards;
+    animation-delay: 1.4s
+  }
+  
+  @keyframes fadeInAfterPoke {
+    0% {
+      opacity: 0;
+      scale: 0.1;
+    }
+    100% {
+      opacity: 1;
+      scale: 1;
+    }
+  }
+
+  .text-scroll-bg {
+    background-color: rgba(250, 250, 250, 0.9);
+    width: 1920px;
+    height: 100%;
+
+    padding-top: 30px;
+    padding-bottom: 30px;
+    margin-bottom: 0%;
+    align-content: center;
+
+    animation: textUP 1.5s 1 forwards;
+      }
+
+  @keyframes textUP {
+    0% {
+      transform: translateY(100%);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0%);
+      opacity: 1;
+    }
+  }
+
+  .vertical {
+    max-width: 900px;
+    max-height: 900px;
+    opacity: 0;
+  }
+  
+  .fade-container {
+      animation: FadeIn 1.5s 1 forwards;
+  }
+
+  .home {
+
+    margin-bottom: 0%;
+    overflow: hidden;
+    background-color: rgba(65, 65, 65, 0.9)
+  }
+
+  @keyframes FadeIn {
+    0%{
+      opacity: 0
+    }
+    
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .upright {
+    max-width: 1100px;
+    animation: PokeIn 2s 1 forwards;
+    position: relative;
+    z-index: 0;
+  }
+
+
+  @keyframes PokeIn {
+    0% {
+      transform: translateY(100%);
+
+    }
+    
+    33% {
+      transform: translateY(-20%);     
+      transform: rotate(-180deg); 
+       }
+
+    66% {
+      transform: translateY(0%);
+    }
+    100% {
+      transform: translateY(-120%)
+    }
+
+  }
+
+ #navbar {
+  z-index: 1;
+  width: 20%;
+  position: fixed;
+  animation: navFill 1.5s 1 forwards;
+}
+
+@keyframes navFill {
+  0% {
+    width: 25%;
+  }
+
+
+  100% {
+    width: 100%;
+  }
+}   
+</style>
+
 <script>
 // @ is an alias to /src
-import CardItem from "@/components/CardItem.vue";
 import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "Home",
   components: {
-    Navbar,
-    CardItem
+    Navbar, 
+  },
+  el:'#home',
+  data () {
+    return {
+      clicked: true
+    }
   }
 };
 </script>
-
-<style scoped>
-  .container {
-    margin-top: 10px;
-  }
-  .bg {
-
-    padding-top: 8%;
-    padding-bottom: 8%;
-    /* The image used */
-    background-image: url('../assets/home-background.jpg');
-
-    /* Full height */
-    height: 100%; 
-
-    /* Center and scale the image nicely */
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-
-    align-content: center;
-  }
-
-  .spacer {
-    padding-top: 5%;
-    padding-bottom: 5%;
-
-
-    background-color: rgba(10, 10, 10, .5);
-  }
-</style>
-
