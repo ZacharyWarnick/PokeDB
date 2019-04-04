1b4a04e492b28404b2e9b515c6efde78f624e5d2
enchang <ianchang@utexas.edu>
2019-04-04 18:26:23 -0500

    Add an alternative code for api.py

--- idb/api.py
+++ idb/api.py
@@ -1,11 +1,13 @@
+#from ??????????? import Pokemon, Evolutions, Types, Forms, BaseStats
 from flask import Blueprint, request, jsonify, session
 from sqlalchemy import create_engine
 
+# So I pretty much made two options because I wasn't sure
+# The first one starts here and there will be a comment break
+# for the next option
+
 database = create_engine('sqlite:???????????')
 app = Flask(__name__)
-api = Blueprint('api', 'api', url_prefix='/api')
-
-@api.route('/????????????', methods=['GET'])
 
 class Pokemon:
     def get(self):
@@ -38,5 +40,56 @@ class BaseStats:
         return {'Base_Stats': [i[0] for i in query.cursor.fetchall()]}
 
 
-if __name__ == '__main__':
-     app.run()
+# Everything before this was something I found online and wasn't sure
+# Everything after is another option that follows the original link
+# Jake sent in back-end. It has areas that need to be filled in with ???
+
+api = Blueprint('api', 'api', url_prefix='/api')
+
+@api.route('/????????????', methods=['GET'])
+
+def api_pokemon():
+    pokemon = request.get_json()
+    pokemons = ???????????????.query.filter_by(status=1).join(Pokemon).order_by(??????????.id.desc()).all()
+    all_pokemons = {'Pokemon':  [pokemon.to_json() for pokemon in pokemons]}
+
+    return jsonify(all_pokemon)
+
+@api.route('/????????????', methods=['GET'])
+
+def api_evolutions():
+    evolution = request.get_json()
+    evolutions = ???????????????.query.filter_by(status=1).join(Evolution).order_by(??????????.id.desc()).all()
+    all_evolutions = {'Pokemon':  [evolution.to_json() for evolution in evolutions]}
+
+    return jsonify(all_evolutions)
+
+@api.route('/????????????', methods=['GET'])
+
+def api_types():
+    type = request.get_json()
+    types = ???????????????.query.filter_by(status=1).join(Types).order_by(??????????.id.desc()).all()
+    all_types = {'Type':  [type.to_json() for type in types]}
+
+    return jsonify(all_types)
+
+@api.route('/????????????', methods=['GET'])
+
+def api_forms():
+    form = request.get_json()
+    forms = ???????????????.query.filter_by(status=1).join(Forms).order_by(??????????.id.desc()).all()
+    all_forms = {'Form':  [form.to_json() for form in forms]}
+
+    return jsonify(all_forms)
+
+@api.route('/????????????', methods=['GET'])
+
+def api_base_stats():
+    base_stat = request.get_json()
+    base_stats = ???????????????.query.filter_by(status=1).join(BaseStats).order_by(??????????.id.desc()).all()
+    all_base_stats = {'Form':  [form.to_json() for form in forms]}
+
+    return jsonify(all_base_stats)
+
+# if __name__ == '__main__':
+#      app.run()
