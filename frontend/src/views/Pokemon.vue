48e7c522b2b5d1d8c5b52ed0dcd8a91d68dba7b4
Jacob Lubecki <jacoblubecki@gmail.com>
2019-04-25 20:47:15 -0500

    Update style for Pokemon binding

--- frontend/src/views/Pokemon.vue
+++ frontend/src/views/Pokemon.vue
@@ -1,69 +1,60 @@
 <template>
   <div class="pokemon">
     <Navbar />
-    <div class="bg">
+    <b-container id="main-content">
+      <img class="bg" src="../assets/home-background.jpg" />
       <div class="container text-scroll-bg">
-      <b-row>
-        <b-col  md="6">
-          <div class="profile-wrapper wow">
-            <div class="d-flex justify-content-between align-items-center">
-              <div>
-                <h2>
-                  <strong>{{ pokemon.name }}</strong>
-                  {{pokemon.id}}
-                </h2>
-                <h2 v-if="pokemon.second_type != null">
-                  {{capitalize(pokemon.first_type.identifier)}}
-                  {{capitalize(pokemon.second_type.identifier)}}
-                </h2>
-                <h2 v-else>
-                  {{capitalize(pokemon.first_type.identifier)}}
-                  {{chainID.base_pokemon}}
-                </h2>
+        <b-container>
+          <b-row class="justify-content-between">
+            <b-col cols="12" md="6">
+              <div class="profile-wrapper wow">
+                <div class="d-flex align-items-center">
+                  <div>
+                    <h2>
+                      <strong>{{ pokemon.name }}</strong>
+                      {{ pokemon.id }}
+                    </h2>
+                    <h2 v-if="pokemon.second_type != null">
+                      {{ capitalize(pokemon.first_type.identifier) }}
+                      {{ capitalize(pokemon.second_type.identifier) }}
+                    </h2>
+                    <h2 v-else>
+                      {{ capitalize(pokemon.first_type.identifier) }}
+                      {{ chainID.base_pokemon }}
+                    </h2>
+                  </div>
+                </div>
+                <p class="poke-desc">
+                  {{ pokemon.flavor_text }}
+                </p>
               </div>
-            </div>
-            <p class="poke-desc">
-              {{ pokemon.flavor_text }}
-            </p>
-        </b-col>
-        <b-col md="6" >
-          <img v-bind:src="pokemon.image" /> 
-        </b-col>
-        </b-row>
+            </b-col>
+            <b-col cols="12" md="4">
+              <img
+                style="max-width: 100%; height: auto;"
+                v-bind:src="pokemon.image"
+              />
+            </b-col>
+          </b-row>
+        </b-container>
         <hr />
         <b-row>
           <b-col md="6">
             <h2>Base Stats</h2>
             <ul class="stats-list">
-              <li>
-                Attack: {{pokemon.base_stats.attack}}
-              </li>
-              <li>
-                Defense: {{pokemon.base_stats.defense}}
-              </li>
-              <li>
-                HP: {{pokemon.base_stats.hp}}
-              </li>
-              <li>
-                Special Attack: {{pokemon.base_stats.special_attack}}
-              </li>
-              <li>
-                Special Defense: {{pokemon.base_stats.special_defense}}
-              </li>
-              <li>
-                Speed: {{pokemon.base_stats.speed}}
-              </li>
+              <li>Attack: {{ pokemon.base_stats.attack }}</li>
+              <li>Defense: {{ pokemon.base_stats.defense }}</li>
+              <li>HP: {{ pokemon.base_stats.hp }}</li>
+              <li>Special Attack: {{ pokemon.base_stats.special_attack }}</li>
+              <li>Special Defense: {{ pokemon.base_stats.special_defense }}</li>
+              <li>Speed: {{ pokemon.base_stats.speed }}</li>
             </ul>
           </b-col>
           <b-col md="6">
             <h2>Identifying Features</h2>
             <ul class="stats-list">
-              <li>
-                Color: {{capitalize(pokemon.color)}}
-              </li>
-              <li class="genus">
-                Genus: {{pokemon.genus}}
-              </li>
+              <li>Color: {{ capitalize(pokemon.color) }}</li>
+              <li>Genus: {{ pokemon.genus }}</li>
 
               <li v-if="pokemon.has_alt_form == true">
                 Alternate Form: Yes
@@ -72,40 +63,30 @@
                 Alternate Form: None
               </li>
 
-              <li>
-                Generation: {{pokemon.since_gen}}
-              </li>
+              <li>Generation: {{ pokemon.since_gen }}</li>
             </ul>
           </b-col>
         </b-row>
-        <hr>
-        <h2  v-if="pokemon.evolution_chain_id != null" class="evo-header">{{pokemon.name}} is a part of this chain:</h2>
-        <EvolutionOverview v-if="pokemon.evolution_chain_id != null"
+        <hr />
+        <h2 v-if="pokemon.evolution_chain_id != null" class="evo-header">
+          {{ pokemon.name }} is a part of this chain:
+        </h2>
+        <EvolutionOverview
+          v-if="pokemon.evolution_chain_id != null"
           :id="chainID"
-          :page_name="pokemon.name"
+          :page_name="pokemon.identifier"
         />
         <h2 v-else>This pokémon is not part of an evolution chain.</h2>
       </div>
-    </div>
+    </b-container>
   </div>
-    
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
 import EvolutionOverview from "@/components/EvolutionOverview.vue";
-import { getPokemon }from "@/api";
+import { getPokemon } from "@/api";
 import { getEvolution } from "@/api";
-
-//Take last element of the url path and request it from the api
-function getID() {
-
-  var pathArray = window.location.pathname.split('/');
-
-  var lastLocation = pathArray.slice(-1).pop();
-
-  return String(lastLocation);
-}
 
 export default {
   name: "Pokemon",
@@ -113,11 +94,11 @@ export default {
     Navbar,
     EvolutionOverview
   },
-    data () {
+  data() {
     return {
       pokemon: null,
       chainID: null
-    }
+    };
   },
 
   methods: {
@@ -127,23 +108,20 @@ export default {
     }
   },
   mounted() {
-    getPokemon(getID()).then(
-      response => (this.pokemon = response.data) && 
-      (this.chainID = response.data.evolution_chain_id)
+    getPokemon(this.$route.params.name).then(
+      response =>
+        (this.pokemon = response.data) &&
+        (this.chainID = response.data.evolution_chain_id)
     );
   }
 };
 </script>
 
 <style scoped>
-
 .section-padding {
   padding: 15px 0;
 }
 
-.genus {
-  text-decoration: line-through;
-}
 .type-badge {
   max-height: 32px;
   height: 100%;
@@ -153,15 +131,16 @@ export default {
 
 .stats-list {
   list-style-type: disc;
-  float:left;
+  float: left;
   margin-left: 2px;
   padding-left: 50px;
   font-size: 19px;
   font-weight: lighter;
   text-align: justify;
-  word-wrap:break-word;
+  word-wrap: break-word;
 }
 .poke-desc {
+  text-align: left;
   padding: 20px 0;
 }
 
@@ -179,39 +158,5 @@ export default {
 
 .evo-header {
   padding: 20px 0px;
-}
-.text-scroll-bg {
-  background-color: white;
-  width: 1920px;
-  height: 100%;
-
-  padding-top: 8vh;
-  padding-bottom: 30px;
-  margin-bottom: 0px;
-  
-  align-content: center;
-  box-shadow: 4px 4px 4px;
-
-}
-.bg {
-  padding-top: 0;
-
-  margin-bottom: 0px;
-
-  /* The image used */
-  background-image: url("../assets/home-background.jpg");
-
-  /* Full height */
-  height: 100vh !important;
-
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-attachment: fixed;
-
-  align-content: center;
-
-  position: sticky;
 }
 </style>
