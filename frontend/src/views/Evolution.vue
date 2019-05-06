7eafe2ed0b783a722a2a7843260767325b0c21eb
Jacob Lubecki <jacoblubecki@gmail.com>
2019-05-06 10:42:56 -0500

    Fix major UI bugs for merge

--- frontend/src/views/Evolution.vue
+++ frontend/src/views/Evolution.vue
@@ -1,79 +1,115 @@
 <template>
   <div id="evolution">
     <Navbar />
-    <div class="bg">
+    <div class="main-content">
+      <img class="bg" src="../assets/home-background.jpg" />
       <div class="container text-scroll-bg">
         <b-jumbotron
-            :header="'Chain: ' + chain.base_pokemon.name"
-            lead="The first Pokémon in an evolution chain defines the species."
+          :header="chain.base_pokemon.name"
+          lead="Scroll down to find out indformation about individual steps"
         />
-        <div class="overview-container">
-          <h1>Here are all the pokemon in the chain</h1>
-          <h3>Scroll down to find out indformation about individual steps</h3>
-        </div>
-      <hr>
+        <hr />
         <div class="d-flex justify-content-center">
-              <b-row class="align-items-center">
-                <b-col >
-                  <a :href="'/pokemon/' + chain.base_pokemon.id" style="text-decoration:none"> <SpriteBasic class="base"
-                  
-                    v-bind:name="chain.base_pokemon.name"
-                    v-bind:id="chain.base_pokemon.id"
-                    v-bind:types="chain.base_pokemon.first_type.identifier"
-                  /> </a>           
-                </b-col>
+          <b-row class="align-items-center">
+            <b-col>
+              <router-link
+                :to="'/pokemon/' + chain.base_pokemon.id"
+                style="text-decoration:none"
+              >
+                <SpriteBasic
+                  class="base"
+                  v-bind:name="chain.base_pokemon.name"
+                  v-bind:id="chain.base_pokemon.id"
+                  v-bind:types="chain.base_pokemon.first_type.identifier"
+                />
+              </router-link>
+            </b-col>
 
-                <b-col v-for="stage in chain.stages" v-bind:key="stage">       
-                  <a :href="'/pokemon/' + stage.pokemon.id" style="text-decoration:none"><SpriteBasic class="looped"
-                  
+            <b-col v-for="stage in chain.stages" v-bind:key="stage">
+              <router-link
+                :to="'/pokemon/' + stage.pokemon.id"
+                style="text-decoration:none"
+                ><SpriteBasic
+                  class="looped"
+                  v-bind:name="stage.pokemon.name"
+                  v-bind:id="stage.pokemon.id"
+                  v-bind:types="stage.pokemon.first_type.identifier"
+                />
+              </router-link>
+            </b-col>
+          </b-row>
+        </div>
+
+        <hr />
+        <div class="indv-step-container">
+          <h2>Individual Steps</h2>
+          <div class="steps">
+            <b-row v-for="stage in chain.stages" v-bind:key="stage">
+              <b-col md="4">
+                <router-link
+                  :to="'/pokemon/' + stage.pokemon.name"
+                  style="text-decoration:none"
+                >
+                  <SpriteBasic
+                    class="looped"
+                    v-bind:name="stage.evolves_from.name"
+                    v-bind:id="stage.evolves_from.id"
+                    v-bind:types="stage.evolves_from.first_type.identifier"
+                  />
+                </router-link>
+              </b-col>
+              <b-col sm="1">
+                <img
+                  src="@/assets/arrow-png-icon-1.jpg"
+                  width="75px"
+                  class="arrow"
+                />
+              </b-col>
+              <b-col md="4">
+                <router-link
+                  :to="'/pokemon/' + stage.pokemon.identifier"
+                  style="text-decoration:none"
+                >
+                  <SpriteBasic
+                    class="looped"
                     v-bind:name="stage.pokemon.name"
                     v-bind:id="stage.pokemon.id"
                     v-bind:types="stage.pokemon.first_type.identifier"
-                  /> </a>
-                </b-col>
-              </b-row>
+                  />
+                </router-link>
+              </b-col>
+              <b-col sm="3" class="test-left">
+                <ul class="list-unstyled requirements-list text-left">
+                  <li>
+                    <strong>Trigger:</strong> {{ capitalize(stage.trigger) }}
+                  </li>
+                  <li v-if="stage.trigger_item != null">
+                    <strong>Trigger Item:</strong>
+                    {{ capitalize(stage.trigger_item) }}
+                  </li>
+                  <li v-if="stage.time_of_day != null">
+                    <strong>Time of Day:</strong> {{ stage.time_of_day }}
+                  </li>
+                  <li v-if="stage.affection != null">
+                    <strong>Required Affection:</strong> {{ stage.affection }}
+                  </li>
+                  <li v-if="stage.beauty != null">
+                    <strong>Required Beauty:</strong> {{ stage.beauty }}
+                  </li>
+                  <li v-if="stage.happiness != null">
+                    <strong>Required Happiness:</strong> {{ stage.happiness }}
+                  </li>
+                  <li v-if="stage.location != null">
+                    <strong>Location:</strong> {{ stage.location }}
+                  </li>
+                  <li><strong>Difficulty:</strong> {{ stage.difficulty }}</li>
+                </ul>
+              </b-col>
+              <hr />
+            </b-row>
           </div>
-      <hr>
-      <div class="indv-step-container">
-        <h2>Individual Steps</h2>
-        <div class="steps">
-          <b-row v-for="stage in chain.stages" v-bind:key="stage">
-            <b-col md="4">
-              <a :href="'/pokemon/' + stage.pokemon.name" style="text-decoration:none"> 
-              <SpriteBasic class="looped"
-                v-bind:name="stage.evolves_from.name"
-                v-bind:id="stage.evolves_from.id"
-                v-bind:types="stage.evolves_from.first_type.identifier"
-              /> 
-              </a>           
-            </b-col>
-            <b-col sm="1">
-              <img src="@/assets/arrow-png-icon-1.jpg" width="75px" class="arrow">
-            </b-col>
-            <b-col md="4">
-              <a :href="'/pokemon/' + stage.pokemon.identifier" style="text-decoration:none"> 
-              <SpriteBasic class="looped"
-                v-bind:name="stage.pokemon.name"
-                v-bind:id="stage.pokemon.id"
-                v-bind:types="stage.pokemon.first_type.identifier"
-              /> 
-              </a>           
-            </b-col>
-            <b-col sm="3">
-              <ul class="list-unstyled requirements-list">
-                <li>Trigger: {{capitalize(stage.trigger)}}</li>
-                <li v-if="stage.trigger_item != null">Trigger Item: {{capitalize(stage.trigger_item)}}</li>
-                <li v-if="stage.time_of_day != null"> Time of Day: {{stage.time_of_day}} </li>
-                <li v-if="stage.affection != null">Required Affection: {{stage.affection}} </li>
-                <li v-if="stage.beauty != null">Required Beauty: {{stage.beauty}} </li>
-                <li v-if="stage.happiness != null">Required Happiness: {{stage.happiness}} </li>
-                <li>Difficulty: {{stage.difficulty}} </li>
-              </ul>
-            </b-col>
-            <hr>
-          </b-row>
         </div>
-       </div>
+      </div>
     </div>
   </div>
 </template>
@@ -81,14 +117,12 @@
 <script>
 import Navbar from "@/components/Navbar.vue";
 import SpriteBasic from "@/components/SpriteBasic.vue";
-import { getPokemon }from "@/api";
 import { getEvolution } from "@/api";
 
 function getID() {
+  var pathArray = window.location.pathname.split("/");
 
-  var pathArray = window.location.pathname.split('/');
-
-  var lastLocation = pathArray.slice(-1).pop()
+  var lastLocation = pathArray.slice(-1).pop();
 
   return String(lastLocation);
 }
@@ -100,76 +134,40 @@ export default {
     SpriteBasic
   },
   methods: {
-  capitalize(s) {
-    if (typeof s !== "string") return "";
-    return s.charAt(0).toUpperCase() + s.slice(1);
-  }
+    capitalize(s) {
+      if (typeof s !== "string") return "";
+      return s.charAt(0).toUpperCase() + s.slice(1);
+    }
   },
   data() {
     return {
-      chain :null
-    }
+      chain: null
+    };
   },
   mounted() {
-    getEvolution(getID()).then(
-      response => (this.chain = response.data)
-    )
+    getEvolution(getID()).then(response => (this.chain = response.data));
   }
 };
 </script>
 
 <style scoped>
-.section-padding {
-  padding: 15px 0;
-}
-
-.text-scroll-bg {
-  background-color: white;
-  width: 1920px;
-  height: 1500px;
-
-  padding-top: 30px;
-  padding-bottom: 30px;
-  align-content: center;
-  box-shadow: 4px 4px 4px;
-}
-.bg {
-  padding-top: 0;
-
-  margin-bottom: 0px;
-
-  /* The image used */
-  background-image: url("../assets/home-background.jpg");
-
-  /* Full height */
-  height: 100%;
-
-  /* Center and scale the image nicely */
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-attachment: fixed;
-
-  align-content: center;
-
-  position: sticky;
-}
 .arrow {
   width: 75px;
   transform: translateX(-25px) translateY(40px);
 }
 
-.overview-container{
+.overview-container {
   background-color: rgb(228, 228, 228);
   padding: 25px;
   margin-bottom: 50px;
 }
+
 .indv-step-container {
   padding: 20px 0px;
   align-content: center;
 }
 
-.steps{
+.steps {
   padding: 50px;
 }
 
